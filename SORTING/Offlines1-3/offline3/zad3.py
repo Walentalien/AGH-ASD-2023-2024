5409f340877c800741dcbc76e076deb1720b3b54
Walentyn <my.mal@gmail.com>
2024-08-10 16:15:47 +0200

    pain bitter pain Lord mercy

--- SORTING/Offlines1-3/offline3/zad3.py
+++ SORTING/Offlines1-3/offline3/zad3.py
@@ -1,38 +1,23 @@
 from zad3testy import runtests
 
 
-
-def fenwick_update(bit, idx, value, n):
-    while idx <= n:
-        bit[idx] += value
-        idx += idx & -idx
-
-def fenwick_query(bit, idx):
-    sum_ = 0
-    while idx > 0:
-        sum_ += bit[idx]
-        idx -= idx & -idx
-    return sum_
-
-def dominance(P):
+#O(n^2)
+def dominance1(P):
     n = len(P)
-    
-    # Sort points by x coordinate
-    P.sort(key=lambda p: p[0])
-    
-    # Initialize Fenwick Tree (BIT) for y-coordinates
-    bit = [0] * (n + 1)
-    
     max_strength = 0
-    # Traverse points
-    for x, y in P:
-        # Query the number of points with y' < y
-        strength = fenwick_query(bit, y - 1)
-        # Update the maximum strength
-        max_strength = max(max_strength, strength)
-        # Update BIT with current y-coordinate
-        fenwick_update(bit, y, 1, n)
-    
+
+    for i in range(n):
+        x_i, y_i = P[i]
+        current_strength = 0
+        
+        for j in range(n):
+            if i != j:
+                x_j, y_j = P[j]
+                if x_i > x_j and y_i > y_j:
+                    current_strength += 1
+        
+        max_strength = max(max_strength, current_strength)
+
     return max_strength
 
 
